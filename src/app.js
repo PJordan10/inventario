8edc530ec93d1bd3b6d985bd26c2ed34eb9aacf9
PJordan10 <jporras1994@gmail.com>
2025-04-24 23:54:37 -0500

    CRUD with Postman

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 /* importamos al framework express */
 import express from "express";
+import cors from "cors";
 import categoriaRoutes from "./routes/categorias.routes.js";
 
 /* Asignamos a app toda funcionalidad para el server web */
@@ -7,6 +8,10 @@ const app = express();
 
 /* setear un puerto al web server */
 app.set("port", 5000);
+
+app.use(express.json()); // habilitar el server para recibir json
+
+app.use(cors()); // habilitar cors para que el cliente pueda acceder a la api
 
 /* routes */
 app.use("/api/categorias", categoriaRoutes);
--- src/routes/categorias.routes.js
+++ src/routes/categorias.routes.js
@@ -5,7 +5,11 @@ import { methodHTTP as categoriaController } from "../controllers/categoria.cont
 const router = Router();
 
 /* configuramos respuesta desde server metodo http get */
-router.get("/", categoriaController.getCategorias)
+router.get("/", categoriaController.getCategorias);
+router.post("/", categoriaController.postCategorias);
+router.get("/:id", categoriaController.getCategory); // obtener una categoria por id
+router.delete("/:id", categoriaController.deleteCategory); // eliminar una categoria por id
+router.put("/:id", categoriaController.updateCategorias); // actualizar una categoria por id
 
 /* hacemos disponible router en toda la app */
 export default router;
