9b7c014edd86ea47c50a7207c2acab97fe9a74e8
PJordan10 <jporras1994@gmail.com>
2025-05-10 11:15:11 -0500

    response a endpoint raiz /

--- src/app.js
+++ src/app.js
@@ -22,5 +22,9 @@ app.use("/api/empleados", empleadosRoutes);
 app.use("/api/clientes", clientesRoutes);
 app.use("/api/productos", productosRoutes);
 
+app.get("/", (req, res) => {
+    res.send("REST API EN RENDER WITHOUT DATABASE ENDPOINTS");
+})
+
 /* hacemos disponible al server app para toda la aplicacion */
 export default app;
